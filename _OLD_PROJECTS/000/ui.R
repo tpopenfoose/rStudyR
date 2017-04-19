360c1b518ea81f36b2017427a33c09ecbfb65837
mihawk <mihawk20101@sina.com>
2017-04-20 01:27:16 +0800

    clear up

--- _OLD_PROJECTS/000/ui.R
+++
@@ -1,3 +0,0 @@
-shinyUI(fluidPage(
-
-))
--- _OLD_PROJECTS/Accretion v1/TOOLS/CLASS6.R
+++ _OLD_PROJECTS/Accretion v1/TOOLS/CLASS6.R
@@ -1,14 +1,22 @@
 require(R6)
+require(compiler)
+XXX <- cmpfun(function() print('TESTFUN'))
 
 R6C_CLASSNAME <- R6Class(
   classname = 'CLASSNAME',
   public = list(
     initialize = function() {
       ## ToDo
-    }
-    
+    },
+    TTT = XXX
   ),
   private = list(
     ## ToDo
   )
 )
+
+
+ABC <- R6C_CLASSNAME$new()
+ABC$TTT
+print(XXX)
+XXX
